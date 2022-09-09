e4d2c8b3c781e214bfeaee131314cd3ac8cbb6f5
arozputnii <a.rozputnii@gmail.com>
2022-09-09 14:36:02 +0300

    import csv to db

---
+++ 01-olympic-games/lib/db_connector.js
@@ -0,0 +1,41 @@
+import db from '../config/database.js'
+
+export default class DbConnector {
+  constructor (record) {
+    this.record = record
+  }
+
+  static getRecord (query) {
+    return new Promise(function (resolve) {
+      db.get(query, function (err, row) {
+        if (err) {
+          return console.error(err.message)
+        } else {
+          resolve(new DbConnector(row || null))
+        }
+      })
+    })
+  }
+
+  async setRecord (createQuery, updateQuery = null, updateCondition = null) {
+    if (this.record && updateQuery && updateCondition) {
+      this.record = { id: await this._runToDb(updateQuery) }
+    } else if (!this.record) {
+      this.record = { id: await this._runToDb(createQuery) }
+    }
+    return this.record
+  }
+
+  _runToDb (query) {
+    return new Promise(function (resolve) {
+      db.serialize(function () {
+        db.run(query, function (err) {
+          if (err) {
+            return console.error(err.message)
+          }
+          resolve(this.lastID)
+        })
+      })
+    })
+  }
+}
